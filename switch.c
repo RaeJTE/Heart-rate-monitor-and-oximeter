2724705346ae7d5176bd5e1320d857e602ec22ac
RaeJTE <jacob.rae@students.plymouth.ac.uk>
2025-01-26 20:37:56 +0000

    Four button initialisation
    Four button initialisation. Note for self in future, when using home computer need to add path to Source Group 1 to the STM support files in the same file as the main code to get STM32f4xx.h file.

--- switch.c
+++ switch.c
@@ -19,35 +19,34 @@ void BLU_BTN_INIT(GPIO_TypeDef *PORT, unsigned int BIT)
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
 	
 	//Configure port
-	PORT->MODER |= (0UL << (2*BIT));		//Set BIT to output mode
+	PORT->MODER |= (0UL << (2*BIT));		//Set BIT to input mode
 	PORT->OSPEEDR |= (0UL << (2*BIT));	//Make BIT Speed 2Mhz to save power
 	PORT->PUPDR |= (3UL <<(2*BIT));			// Turn off Pullup and Pull down resistors for BIT
+}
+
+void FOUR_BTN_INIT(GPIO_TypeDef *PORT, unsigned int BIT0, unsigned int BIT1, unsigned int BIT2, unsigned int BIT3)
+{
+	FOUR_BTN_PORT_ENABLE();	//Activates the port for the four directional buttons
+	//Sets the pins for the four buttons to be inputs
+	set_BTN_input(BIT0);
+	set_BTN_input(BIT1);
+	set_BTN_input(BIT2);
+	set_BTN_input(BIT3);
+	//Sets the pins for the four buttons to be low speed to conserve power
+	set_BTN_lowspeed(BIT0);
+	set_BTN_lowspeed(BIT1);
+	set_BTN_lowspeed(BIT2);
+	set_BTN_lowspeed(BIT3);
+	//Sets the pins for the four buttons to not use internal pull-up or pull-down resistors
+	set_BTN_not_pull_up(BIT0);
+	set_BTN_not_pull_up(BIT1);
+	set_BTN_not_pull_up(BIT2);
+	set_BTN_not_pull_up(BIT3);
 }
 
 void Toggle_LED(void)
 {
 	GPIOB->ODR^=(0b00000001<<LED_GRN);								//Toggles green LED
-}
-
-void switchInit(void)
-{
-	switch_PORT_ENABLE();	//Clocks the port being used for inputs to enable it
-	//Button 0
-	set_BTN_input(BTN0_PIN);	//Sets to input mode
-	set_BTN_lowspeed(BTN0_PIN);	//Sets to 2MHz (low speed) operation to save power
-	set_BTN_pull_up(BTN0_PIN);	//Sets to pull up configuration
-	//Button 1
-	set_BTN_input(BTN1_PIN);	//Sets to input mode
-	set_BTN_lowspeed(BTN1_PIN);	//Sets to 2MHz (low speed) operation to save power
-	set_BTN_pull_up(BTN1_PIN);	//Sets to pull up configuration
-	//Button 2
-	set_BTN_input(BTN2_PIN);	//Sets to input mode
-	set_BTN_lowspeed(BTN2_PIN);	//Sets to 2MHz (low speed) operation to save power
-	set_BTN_pull_up(BTN2_PIN);	//Sets to pull up configuration
-	//Button 3
-	set_BTN_input(BTN3_PIN);	//Sets to input mode
-	set_BTN_lowspeed(BTN3_PIN);	//Sets to 2MHz (low speed) operation to save power
-	set_BTN_pull_up(BTN3_PIN);	//Sets to pull up configuration
 }
 
 int readBTNValue(GPIO_TypeDef *PORT, unsigned int BIT) //Reads whether a given button is being pressed - made easier by the 4 buttons using pins 0,1,2,3
--- switch.h
+++ switch.h
@@ -19,17 +19,18 @@
 	#define BTN2_PIN	2	//Defines pin used by button 2
 	#define BTN3_PIN	3	//Defines pin used by button 3
 //Initialisation commands
-#define switch_PORT_ENABLE() 		RCC->AHB1ENR 					|= RCC_AHB1ENR_GPIOGEN							//Defines command to clock to switch port (port G) to enable it
+#define FOUR_BTN_PORT_ENABLE() 		RCC->AHB1ENR 					|= RCC_AHB1ENR_GPIOGEN							//Defines command to clock to four button port (port G) to enable it
 
-#define set_BTN_input(BTN_num)				switch_PORT->MODER 		|= 0<<(2*BTN_num)	//Defines command to set a button of given number to input mode
-#define set_BTN_lowspeed(BTN_num)			switch_PORT->OSPEEDR 	|= 0<<(2*BTN_num)	//Defines command to set a button of given number to 2MHz (low speed) operation to save power
-#define set_BTN_pull_up(BTN_num)			switch_PORT->PUPDR 		|= 1<<(2*BTN_num)	//Defines command to set a button of given number to internal pull up configuration
+#define set_BTN_input(BTN_num)				switch_PORT->MODER 		|= 0ul<<(2*BTN_num)	//Defines command to set a button of given number to input mode
+#define set_BTN_lowspeed(BTN_num)			switch_PORT->OSPEEDR 	|= 0ul<<(2*BTN_num)	//Defines command to set a button of given number to 2MHz (low speed) operation to save power
+#define set_BTN_not_pull_up(BTN_num)			switch_PORT->PUPDR 		|= 3ul<<(2*BTN_num)	//Defines command to set a button of given number to not use internal pull up configuration
 //Activation commands
 #define LEDon(PORT, BIT)   ((PORT->ODR) |= (1U << BIT))		// Macro to set port bit - not BSRR (Here to test responsiveness of buttons)
 #define LEDoff(PORT, BIT)   ((PORT->ODR) &= ~(1U << (BIT))) // Macro to clear port bit - not BSRR (Here to test responsiveness of buttons)
 
 void LED_INIT(GPIO_TypeDef *PORT, unsigned int BIT);	//Initialises green traffic LED
 void BLU_BTN_INIT(GPIO_TypeDef *PORT, unsigned int BIT);	//Initialises Blue button
+void FOUR_BTN_INIT(GPIO_TypeDef *PORT, unsigned int BIT0, unsigned int BIT1, unsigned int BIT2, unsigned int BIT3);	//Initialises the four directional buttons
 void Toggle_LED (void); //Toggles green LED
 void switchInit(void);	//Initialises 4 buttons
 int readBTNValue(GPIO_TypeDef *PORT, unsigned int BIT);	//Reads whether a specified 1 of the 4 buttons has been pressed - made easier by the 4 buttons using pins 0,1,2,3
