e3544052a8bb82ad21b86d4070b599a0303c4e3d
RaeJTE <147714975+RaeJTE@users.noreply.github.com>
2025-02-17 15:37:45 +0000

    No phantom red LED
    The red LED uses the same pin as DAC 2 and previously we were seeing it turn on when we didn't want it to because of this. Use of DAC 2 has been removed as it was deemed unlikely to be necessary in the final project anyway.

--- DAC.c
+++ DAC.c
@@ -5,11 +5,11 @@ void init_DAC(void)
 {
 	RCC->AHB1ENR|=RCC_AHB1ENR_GPIOAEN;			//DAC port clock enable
 	DAC_port->MODER|=(3u<<(2*DAC1_pin));			//DAC1 output pin set as anaglogue
-	DAC_port->MODER|=(3u<<(2*DAC2_pin));			//DAC2 output pin set as anaglogue
+	//DAC_port->MODER|=(3u<<(2*DAC2_pin));			//DAC2 output pin set as anaglogue
 	
 	RCC->APB1ENR|=RCC_APB1ENR_DACEN;				//DAC clock enable
 	DAC->CR|=DAC_CR_EN1;										//DAC1 enabled
-	DAC->CR|=DAC_CR_EN2;										//DAC2 enabled
+	//DAC->CR|=DAC_CR_EN2;										//DAC2 enabled
 }
 
 void output_dac1(unsigned short d)
--- DAC.h
+++ DAC.h
@@ -5,11 +5,13 @@
 
 #define DAC_port	GPIOA
 #define DAC1_pin		4
-#define DAC2_pin		5
+//#define DAC2_pin		5
+
+//DAC2 uses the same pin as the red LED on PCB board and so can no longer be used.
 
 void init_DAC(void);
 void output_dac1(unsigned short d);
-void output_dac2(unsigned short d);
+//void output_dac2(unsigned short d);
 
 #endif
 
